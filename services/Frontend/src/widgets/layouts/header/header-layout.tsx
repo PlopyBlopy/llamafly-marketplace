d9dc0dd31a98dbb0aa07b1558c80c81efcb71d77
Aleksei <TheIcebreakerBreaksTheIce@gmail.com>
2025-07-29 23:34:23 +0300

    feat(Frontend): header - компонент, дополненый базовыми версиями требуемых компонентов #23 1. Все компоненты без вызовов api
    ## Компоненты
    - Добавлен логотип сайта с переходом на MainPage (LogoRouter)
    - Добавленно меню категорий (CategoriesMenu)
    - Добавлен поле ввода для поиска товаров на сайте (SearchHeaderContainer)
    - Добавлен контейнер авторизации что включает кнопки для логина и регистрации (AuthorizationContainer)

--- services/Frontend/src/widgets/layouts/header/header-layout.tsx
+++ services/Frontend/src/widgets/layouts/header/header-layout.tsx
@@ -1,7 +1,17 @@
+import styles from "./header-layout.module.css";
+
+import { LogoRouter } from "@/features/logo-router";
+import { CategoriesMenu } from "@/widgets/categories-menu";
+import { SearchHeaderContainer } from "@/widgets/search-container";
+import { AuthorizationContainer } from "@/widgets/authorization-container";
+
 export const HeaderLayout = () => {
   return (
-    <>
-      <h1>Header</h1>
-    </>
+    <div className={styles.container}>
+      <LogoRouter />
+      <CategoriesMenu />
+      <SearchHeaderContainer />
+      <AuthorizationContainer />
+    </div>
   );
 };
