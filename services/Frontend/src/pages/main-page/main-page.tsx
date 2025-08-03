60da5f5e6248ce0640da55b1d088c9b7639da615
Aleksei <TheIcebreakerBreaksTheIce@gmail.com>
2025-08-03 21:22:28 +0300

    feat(Frontend): добавленно компоненты в MainPage - Добавлен MainPageFilter - Добавлен ProductGrid - Обновлен css стиль

--- services/Frontend/src/pages/main-page/main-page.tsx
+++ services/Frontend/src/pages/main-page/main-page.tsx
@@ -1,7 +1,14 @@
+import { MainPageFilter } from "@/widgets/main-page-filter";
+import styles from "./main-page.module.css";
+import { ProductGrid } from "@/widgets/product-grid";
+
 export const MainPage = () => {
   return (
-    <>
-      <h1>Main Page</h1>
-    </>
+    <div className={styles.container}>
+      <div className={styles.filterContainer}>
+        <MainPageFilter />
+      </div>
+      <ProductGrid />
+    </div>
   );
 };
