281a6051cc68239ef27cd77a5e2369cc97868819
Aleksei <TheIcebreakerBreaksTheIce@gmail.com>
2025-08-05 10:12:05 +0300

    chore(Frontend): изменен компонент, добавлен стиль #32 - Измененн компонент ProductPage - добавлен пустой css стиль

--- services/Frontend/src/pages/product-page/product-page.tsx
+++ services/Frontend/src/pages/product-page/product-page.tsx
@@ -1,7 +1,12 @@
+import { useSearchParams } from "react-router-dom";
+
 export const ProductPage = () => {
+  const [searchParams] = useSearchParams();
+  const query = searchParams.get("id");
+
   return (
     <>
-      <h1>ProductPage</h1>
+      <h1>ProductPage: {query}</h1>
     </>
   );
 };
