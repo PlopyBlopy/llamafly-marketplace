5c5c6d9f1f7b3bb31ef787b4cbd59d0d2f9d3664
Aleksei <TheIcebreakerBreaksTheIce@gmail.com>
2025-08-05 10:05:21 +0300

    refactor(Frontend): изменен текст в FooterLayout close #24 - Изменен текст в соответствии с issue #24 - Изменен стиль  текста

--- services/Frontend/src/widgets/layouts/footer/footer-layout.tsx
+++ services/Frontend/src/widgets/layouts/footer/footer-layout.tsx
@@ -3,7 +3,8 @@ import styles from "./footer-layout.module.css";
 export const FooterLayout = () => {
   return (
     <div className={styles.items}>
-      <h1>Footer</h1>
+      <text className={styles.inDevText}>In development. </text>
+      <text className={styles.descriptionText}>Сайт нереального маркетплейса, в качестве практики Backend, Frontend, DevOps.</text>
     </div>
   );
 };
