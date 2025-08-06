5eef8f0cc49632333f70ca906cd3329586ee88fb
Aleksei <TheIcebreakerBreaksTheIce@gmail.com>
2025-08-06 07:16:31 +0300

    refactor(Frontend): изменен footer-layout - изменен компонент, смена элемента text -> p - изменен стиль, добавленны padding, margin

--- services/Frontend/src/widgets/layouts/footer/footer-layout.tsx
+++ services/Frontend/src/widgets/layouts/footer/footer-layout.tsx
@@ -3,8 +3,8 @@ import styles from "./footer-layout.module.css";
 export const FooterLayout = () => {
   return (
     <div className={styles.items}>
-      <text className={styles.inDevText}>In development. </text>
-      <text className={styles.descriptionText}>Сайт нереального маркетплейса, в качестве практики Backend, Frontend, DevOps.</text>
+      <p className={styles.inDevText}>In development. </p>
+      <p className={styles.descriptionText}>Сайт нереального маркетплейса, в качестве практики Backend, Frontend, DevOps.</p>
     </div>
   );
 };
