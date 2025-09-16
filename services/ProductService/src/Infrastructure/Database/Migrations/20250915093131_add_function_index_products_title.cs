caf9e450895e8af2d6195185712e4fcb0a85dceb
Aleksei <TheIcebreakerBreaksTheIce@gmail.com>
2025-09-16 10:22:48 +0300

    refactor(ProductServiceDB): добавлен функциональный индекс LOWER(title) для столбца title, таблицы products

---
+++ services/ProductService/src/Infrastructure/Database/Migrations/20250915093131_add_function_index_products_title.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_function_index_products_title : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("CREATE INDEX idx_title_lower ON products (LOWER(title));");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP INDEX idx_title_lower;");
+        }
+    }
+}
