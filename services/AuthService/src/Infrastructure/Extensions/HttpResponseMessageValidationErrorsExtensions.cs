d8c20d382b3c8fbdd8f55d82420177b712a4eb11
Aleksei <TheIcebreakerBreaksTheIce@gmail.com>
2025-10-03 08:16:20 +0300

    feat(AuthService): extension метод для HttpResponseMessage - для форматирования ошибок валидации,  в типы ValidationField, ValidationFieldError

---
+++ services/AuthService/src/Infrastructure/Extensions/HttpResponseMessageValidationErrorsExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Domain.DTO.Services.ResultsError;
+using FluentResults;
+using FluentResults.Errors;
+using System.Net.Http.Json;
+
+namespace Infrastructure.Extensions
+{
+    internal static class HttpResponseMessageValidationErrorsExtensions
+    {
+        public static async Task<Result<TResponse>> HandleValidationErrorsAsync<TResponse>(this HttpResponseMessage responseMessage, CancellationToken ct)
+        {
+            if (responseMessage == null)
+                return Result.Fail<TResponse>("The response message cannot be null");
+
+            var errorResponse = await responseMessage.Content.ReadFromJsonAsync<ErrorResponse>(ct);
+
+            if (errorResponse?.Reason == null || !errorResponse.Reason.Any())
+                return Result.Fail<TResponse>("Response reasons content empty");
+
+            var errors = errorResponse.Reason
+                .SelectMany(e => e.Reasons)
+                .Select(r => new ValidationFieldError(r.Message, r.Metadata.ErrorCode, r.Metadata.FieldName, r.Metadata.AttemptedValue))
+                .ToList();
+
+            return Result.Fail<TResponse>(new ValidationError(typeof(TResponse).Name.ToString(), errors));
+        }
+    }
+}
