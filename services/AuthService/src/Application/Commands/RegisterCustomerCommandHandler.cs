f45ba4bd8c5424c456a513865c4b6b100233d7f0
Aleksei <TheIcebreakerBreaksTheIce@gmail.com>
2025-10-03 08:21:41 +0300

    feat(AuthService): добавлена логика handlers

--- services/AuthService/src/Application/Commands/RegisterCustomerCommandHandler.cs
+++ services/AuthService/src/Application/Commands/RegisterCustomerCommandHandler.cs
@@ -1,6 +1,10 @@
 ﻿using AutoMapper;
 using Domain.Commands;
+using Domain.Commands.Services;
+using Domain.Interfaces;
 using Domain.Interfaces.Repositories;
+using Domain.Interfaces.Services;
+using Domain.Models.Password;
 using FluentResults;
 using MediatoR.Alternative.Lite;
 
@@ -8,17 +12,36 @@ namespace Application.Commands
 {
     internal sealed class RegisterCustomerCommandHandler : ICommandHandler<RegisterCustomerCommand, RegisterCustomerResponse>
     {
+        private readonly IProfileService _profileService;
         private readonly IAddPasswordRepository _repository;
         private readonly IMapper _mapper;
+        private readonly IPasswordHasher _passwordHasher;
 
-        public RegisterCustomerCommandHandler(IAddPasswordRepository repository, IMapper mapper)
+        public RegisterCustomerCommandHandler(IProfileService profileService, IAddPasswordRepository repository, IMapper mapper, IPasswordHasher passwordHasher)
         {
+            _profileService = profileService;
             _repository = repository;
             _mapper = mapper;
+            _passwordHasher = passwordHasher;
         }
 
         public async Task<Result<RegisterCustomerResponse>> Handle(RegisterCustomerCommand command, CancellationToken ct)
         {
+            var createAdminRequest = _mapper.Map<CreateCustomerRequest>(command);
+
+            var response = await _profileService.CreateCustomerAsync(createAdminRequest, ct);
+
+            if (response.IsFailed)
+            {
+                return Result.Fail<RegisterCustomerResponse>(response.Errors);
+            }
+
+            var userId = response.Value.UserId;
+            var passwordHash = _passwordHasher.Hash(command.User.Password);
+
+            var passwordModel = _mapper.Map<PasswordModel>((userId: userId, passwordHash: passwordHash));
+            var result = await _repository.AddAsync(passwordModel, ct);
+
             return Result.Ok();
         }
     }
