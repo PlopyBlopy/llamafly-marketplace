a04f81bd78d8653455f0dffc841424a3ba263fad
Aleksei <TheIcebreakerBreaksTheIce@gmail.com>
2025-10-04 11:13:08 +0300

    feat(ProfileService): extension метод для FluentResults.Result - Converter

---
+++ services/ProfileService/src/Shared/Extensions/ResultConvert.cs
@@ -0,0 +1,14 @@
+﻿using FluentResults;
+
+namespace Shared.Extensions
+{
+    public static class ResultConvert
+    {
+        public static Result<TOut> Convert<TIn, TOut>(this Result<TIn> result, Func<TIn, TOut> mapper)
+        {
+            return result.IsSuccess
+                ? Result.Ok(mapper(result.Value))
+                : Result.Fail<TOut>(result.Errors);
+        }
+    }
+}
